29f4cfd17da0d5180e14de9d7dc4a1ae06519ba2
fangp <ffgpuboy@gmail.com>
2018-04-10 23:46:51 -0400

    live part
    phantomjs+cheeriojs

--- scraper/score.js
+++ scraper/score.js
@@ -12,17 +12,35 @@ exports.updateScore = async function () {
 
 async function loadPage(url) {
     const instance = await phantom.create();
-    console.log()
     const page = await instance.createPage();
+    console.log(1);
     const status = await page.open(url);
     console.log(status);
     return await page.property('content');
 }
 
 async function scraper(html) {
-    console.log(1);
+    console.log(3);
     const $ = await cheerio.load(html);
-    const teamblocks = $('div[class^="team-game-score"]');
-    //const teamblocks = $('div[class="team-game-score"]')
-    console.log(teamblocks);
+    let matches = {};
+    const teamblocks = $('div[class="score-tile-min__wrapper"]').each(function (i, elem) {
+        let match = {};
+        // ----------------------- names ----------------------------------------------
+        let teamnames = $(this).find('div[class="score-tile__team-name"]').find('span');
+        match['visitor'] = teamnames.eq(0).text();
+        match['home'] = teamnames.eq(1).text();
+        // ----------------------- time -----------------------------------------------
+        let teamtimes = $(this).find('span[class="score-tile__period"]').text().toString().trim().replace(/[\s\n]+/g, ' ');
+        match['time'] = teamtimes;
+        //console.log(teamtimes);
+        // ----------------------- scores ----------------------------------------------
+        let scores = [];
+        let teamscores = $(this).find('span[class="current_score"]');
+        scores[0] = teamscores.eq(0).text();
+        scores[1] = teamscores.eq(1).text();
+        match['score'] = scores;
+
+        matches[i.toString()] = match;
+    });
+    console.log(matches);
 }
